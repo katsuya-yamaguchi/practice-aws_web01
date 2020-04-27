a75c83fcc9e7e1882fc063f82f0dd0178ba9339f
Katsuya003 <bb.win.3@gmail.com>
2020-04-27 10:06:28 +0900

    #15　assets格納用のS3バケットを作成。

--- src/terraform/s3/env/dev/output.tf
+++ src/terraform/s3/env/dev/output.tf
@@ -1,3 +1,6 @@
 output "s3_bucket_logging" {
   value = module.s3.s3_bucket_logging
 }
+output "s3_bucket_assets" {
+  value = module.s3.s3_bucket_assets
+}
--- src/terraform/s3/module/s3/output.tf
+++ src/terraform/s3/module/s3/output.tf
@@ -1,3 +1,6 @@
 output "s3_bucket_logging" {
   value = aws_s3_bucket.logging_katsuya_place_work.bucket
 }
+output "s3_bucket_assets" {
+  value = aws_s3_bucket.assets_katsuya_place_work.bucket
+}
