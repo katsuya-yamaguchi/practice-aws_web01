df4a30f6d40eb5d5a711f42d1c4aced7bf5f4b47
Katsuya003 <bb.win.3@gmail.com>
2020-05-05 14:53:22 +0900

    #18　pumaを起動するようにuserdataを修正。

--- src/terraform/ec2/web/module/ec2/userdata.sh
+++ src/terraform/ec2/web/module/ec2/userdata.sh
@@ -5,5 +5,5 @@ touch $LOG
 chmod 777 $LOG
 echo "START userdata script." >> $LOG
 su - app-user -c 'cd /home/app-user/www/app && bundle exec rake assets:precompile'
-su - app-user -c 'cd /home/app-user/www/app && bundle exec rails db:migrate'
+su - app-user -c 'cd /home/app-user/www/app && bundle exec rails db:migrate && puma -t 5:5 -p 80 -c config/puma.rb'
 echo "END userdata script." >> $LOG
