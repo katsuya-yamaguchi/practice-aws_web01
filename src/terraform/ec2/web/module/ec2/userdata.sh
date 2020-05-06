a93748d4037d7a051dc9ed1e7d4c796275afd89f
Katsuya003 <bb.win.3@gmail.com>
2020-05-07 02:01:00 +0900

    #18　Railsアプリケーションを起動させる処理を追加。

--- src/terraform/ec2/web/module/ec2/userdata.sh
+++ src/terraform/ec2/web/module/ec2/userdata.sh
@@ -6,4 +6,5 @@ chmod 777 $LOG
 echo "START userdata script." >> $LOG
 su - app-user -c 'cd /home/app-user/www/app && bundle exec rake assets:precompile'
 su - app-user -c 'cd /home/app-user/www/app && bundle exec rails db:migrate && puma -t 5:5 -p 80 -c config/puma.rb'
+cd /home/app-user/www/app && /home/app-user/.rbenv/shims/bundle exec puma -t 5:5 -C config/puma.rb -e production -p 80 -d
 echo "END userdata script." >> $LOG
