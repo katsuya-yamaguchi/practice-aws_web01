7376c950856bcda78d1d06b1262b120b94dda5c1
Katsuya003 <bb.win.3@gmail.com>
2020-05-07 23:36:05 +0900

    fix #19　.bash_profileを読み込む処理を追加する。
    userdataの処理中は、.bash_profileを読み込まないので設定していたSECRET_KEY_BASEが反映されず、アプリが500エラーを返す状態だった。
    そのため、読み込む処理を追加してpumaの起動を行うことで解消。

--- src/terraform/ec2/web/module/ec2/userdata.sh
+++ src/terraform/ec2/web/module/ec2/userdata.sh
@@ -3,8 +3,21 @@ LOG=/var/log/user-data.log
 
 touch $LOG
 chmod 777 $LOG
-echo "START userdata script." >> $LOG
-su - app-user -c 'cd /home/app-user/www/app && bundle exec rake assets:precompile'
-su - app-user -c 'cd /home/app-user/www/app && bundle exec rails db:migrate && puma -t 5:5 -p 80 -c config/puma.rb'
-cd /home/app-user/www/app && /home/app-user/.rbenv/shims/bundle exec puma -t 5:5 -C config/puma.rb -e production -p 80 -d
-echo "END userdata script." >> $LOG
+
+echo "=============================" >> $LOG
+echo "= START userdata script." >> $LOG
+echo "=============================" >> $LOG
+
+echo "# START assets:precompile" >> $LOG
+su - app-user -c 'cd /home/app-user/www/app && bundle exec rake assets:precompile' >> $LOG
+echo "# END   assets:precompile" >> $LOG
+
+echo "# START puma" >> $LOG
+cd /home/app-user/www/app
+. ~/.bash_profile
+/home/app-user/.rbenv/shims/bundle exec puma -t 5:5 -C config/puma.rb -e production -p 80 -d >> $LOG
+echo "# END   puma" >> $LOG
+
+echo "=============================" >> $LOG
+echo "= END userdata script." >> $LOG
+echo "=============================" >> $LOG
