63fede09f86d9040d217fde9343100aaaa30ec27
Katsuya003 <bb.win.3@gmail.com>
2020-05-07 12:05:33 +0900

    terrraform fmt

--- src/terraform/network/module/vpc/main.tf
+++ src/terraform/network/module/vpc/main.tf
@@ -94,9 +94,9 @@ resource "aws_route" "bastion_az_a" {
 }
 
 resource "aws_route" "bastion_az_c" {
-  route_table_id = aws_route_table.public_c.id
+  route_table_id         = aws_route_table.public_c.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.igw.id
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 ##################################################
